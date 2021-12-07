a9e5bf36b0cdf18b01e7a3c02434052462ad3cb2
Булат Хабиров <bulyn2001@mail.ru>
2021-12-07 12:43:46 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetworks/Layer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace NeuralNetworks
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get; }
+        public int Count => Neurons?.Count ?? 0;
+        public Layer(List<Neuron> neurons,NeuronType type=NeuronType.Normal) 
+        {
+            Neurons = neurons;
+        }
+        public List<double> GetSignals ()
+        {
+            List<double> result = new List<double>();
+            foreach (var neuron in Neurons)
+            {
+                result.Add(neuron.Output);
+            }
+            return result;
+        }
+    }
+}
